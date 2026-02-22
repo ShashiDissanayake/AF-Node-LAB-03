69e1703c86297ef21f622d34d71e4c9331457583
ShashiDissanayake <sewminishashikala781@gmail.com>
2026-02-22 11:39:35 +0530

    update file content and adjust readFile logging

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
     console.log("Error reading file:", err);
     return;
   }
-
-  console.log("File Content:");
   console.log(data);
+});
+
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+ if (err) throw err;
+ console.log('File saved!');
 });
